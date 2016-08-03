25f71a2b532ac70845a45e5fd49efa798b2c6534
XU Sai <xusai@microduino.cc>
2016-08-03 18:37:40 +0800

    [UI] add the component of material-ui

--- client/main.js
+++ client/main.js
@@ -4,7 +4,8 @@ import {mount} from 'react-mounter';
 
 import AppContainer from '../imports/ui/App.jsx';
 import { render } from 'react-dom';
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
 Meteor.startup(()=>{
   // render(<AppContainer />,document.getElementById('container'))
+  injectTapEventPlugin();
 });
